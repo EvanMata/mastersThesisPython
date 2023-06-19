7a5e412c01bc8cc75cebd2b7783abbc69f3802f9
Evan Mata <eamata@uchicago.edu>
2023-06-19 17:38:42 -0400

    Distance from pure color created and tested, works

---
+++ clustering.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+import openRawData as opn
+import pathlib_variable_names as var_names
+
+from scipy.optimize import minimize
+from sklearn.cluster import SpectralClustering
+
+def distance_from_pure_color(img, pure_colors):
+    img = np.array(img)
+    pure_colors = sorted(pure_colors)
+    color_midpoints = [(pure_colors[i+1] + pure_colors[i]) / 2 
+                       for i in range(len(pure_colors) - 1)]
+    previously_updated = np.zeros(img.shape)
+    closest_colors = np.zeros(img.shape)
+    for i in range(len(color_midpoints)):
+        color_midpoint = color_midpoints[i]
+        currently_updated = np.zeros(img.shape)
+        currently_updated[img < color_midpoint] = 1
+        new_pts = currently_updated - previously_updated
+        closest_colors[new_pts > 0] = pure_colors[i]
+        previously_updated = currently_updated
+    closest_colors[previously_updated == 0] = pure_colors[i + 1]
+    
+    all_large_dists_1 = (color_midpoints - np.array(pure_colors[:-1]))**2
+    all_large_dists_2 = (np.array(pure_colors[1:]) - color_midpoints)**2
+    largest_possi_dist = max([max(all_large_dists_1), max(all_large_dists_2)])
+    
+    dist_pc = (closest_colors - img)**2
+    dist_pc = np.sum(dist_pc) / (img.size * largest_possi_dist)
+    normed_dist_pc = dist_pc 
+    
+    return normed_dist_pc, closest_colors, largest_possi_dist
--- unitTestingPhase2.py
+++ unitTestingPhase2.py
@@ -1,6 +1,71 @@
+import numpy as np
+
+import clustering as clu
 import openRawData as opn
 import pathlib_variable_names as var_names
 
 
+def t_dPC_1():
+    """
+    Test distance from pure color with trivial example 
+    -- PASSED
+    """
+    test_arr = np.array([[1, 0], [0, 1]])
+    pure_colors = [0, 1]
+    normed_dist_pc, closest_colors, largest_possi_dist = \
+        clu.distance_from_pure_color(test_arr, pure_colors)
+    print(normed_dist_pc == 0)
+    print(largest_possi_dist == .5**2)
+    print(closest_colors)
+
+
+def t_dPC_2():
+    """
+    Test distance from pure color with pts not exactly on colors, 
+    and close to both possi pure colors
+    -- PASSED
+    """
+    test_arr = np.array([[.4, .6], [.3, .5]])
+    pure_colors = [0, 1]
+    normed_dist_pc, closest_colors, largest_possi_dist = \
+        clu.distance_from_pure_color(test_arr, pure_colors)
+    print(normed_dist_pc - ((.4**2 + .4**2 + .3**2 + .5**2)/(4*.5**2))**0.5 < 0.01)
+    print(largest_possi_dist == .5**2)
+    print(closest_colors)
+    print()
+
+
+def t_dPC_3():
+    """
+    Test distance from pure color with pts not exactly on colors, 
+    3 colors / multiple midpoints and an unsorted colors group
+    -- PASSED
+    """
+    test_arr = np.array([[.4, .6], [.2, .5]])
+    pure_colors = [0, 1, .5]
+    normed_dist_pc, closest_colors, largest_possi_dist = \
+        clu.distance_from_pure_color(test_arr, pure_colors)
+    print(normed_dist_pc - ((.1**2 + .1**2 + .2**20)/(4*.25**2))**0.5 < 0.01)
+    print(largest_possi_dist == .25**2)
+    print(closest_colors)
+    print()
+    
+    
+def t_dPC_4():
+    """
+    Test distance from pure color with pts not exactly on colors, 
+    3 colors / multiple midpoints where not all midpoints
+    are equadistant from all colors and an unsorted colors group
+    -- PASSED
+    """
+    test_arr = np.array([[.4, .6], [.2, .9]])
+    pure_colors = [0, 1, .6]
+    normed_dist_pc, closest_colors, largest_possi_dist = \
+        clu.distance_from_pure_color(test_arr, pure_colors)
+    print(normed_dist_pc - ((.2**2 + 0**2 + .2**2 + .1**2)/(4*.3**2))**0.5 < 0.01)
+    print(largest_possi_dist == .3**2)
+    print(closest_colors)
+    print()
+
 if __name__ == "__main__":
-    a=5
+    t_dPC_4()
